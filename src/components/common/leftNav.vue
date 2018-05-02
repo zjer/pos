b20036a876d064fa087a10c15c77933c56dedfd6
shijun <jun.shi5@pactera.com>
2018-05-02 18:16:05 +0800

    initial commit

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="left-nav">
+      <ul>
+        <li>
+          <i class="icon iconfont icon-pay"></i>
+          <div>收营</div>
+        </li>
+        <li>
+          <i class="icon iconfont icon-shop"></i>
+          <div>店铺</div>
+        </li>
+        <li>
+          <i class="icon iconfont icon-goods"></i>
+          <div>商品</div>
+        </li>
+        <li>
+          <i class="icon iconfont icon-vip"></i>
+          <div>会员</div>
+        </li>
+        <li>
+          <i class="icon iconfont icon-statistics"></i>
+          <div>统计</div>
+        </li>
+        <li>
+          <i class="icon iconfont icon-settings"></i>
+          <div>设置</div>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'leftNav'
+  }
+</script>
+
+<style>
+  .left-nav {
+    width: 5%;
+    height: 100%;
+    background-color: #1d8ce0;
+    float: left;
+    font-size: 10px;
+    color: #ffffff;
+  }
+  .left-nav ul {
+    margin: 0;
+    padding: 0;
+  }
+  .left-nav ul li {
+    list-style: none;
+    text-align: center;
+    border-bottom: 1px solid #20a0ff;
+  }
+  .iconfont {
+    font-size: 16px;
+  }
+</style>
---
+++ src/components/page/Pos.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="pos">
+    <el-row>
+      <el-col :span="8" class="pos-order" id="orderList">
+        <el-tabs>
+          <el-tab-pane label="点餐">
+            <el-table :data="tableData" border style="width: 100%;">
+              <el-table-column prop="goodsName" label="商品名称"></el-table-column>
+              <el-table-column prop="goodsCount" label="商品数量"></el-table-column>
+              <el-table-column prop="goodsPrice" label="商品单价"></el-table-column>
+              <el-table-column label="操作" fixed="right">
+                <template slot-scope="scope">
+                  <el-button type="text" size="small">删除</el-button>
+                  <el-button type="text" size="small">增加</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+            <div class="btns">
+              <el-button type="warning">挂单</el-button>
+              <el-button type="danger">删除</el-button>
+              <el-button type="primary">结账</el-button>
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="挂单">挂单</el-tab-pane>
+          <el-tab-pane label="外卖">外卖</el-tab-pane>
+        </el-tabs>
+      </el-col>
+      <el-col :span="16">
+        我是产品栏
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  import ElTabPane from "element-ui/packages/tabs/src/tab-pane";
+  import ElButton from "element-ui/packages/button/src/button";
+
+  export default {
+    components: {
+      ElButton,
+      ElTabPane
+    },
+    name: 'Pos',
+    data() {
+      return {
+        tableData: [{
+          goodsName: '可口可乐',
+          goodsPrice: 8,
+          goodsCount:1
+        }, {
+          goodsName: '香辣鸡腿堡',
+          goodsPrice: 15,
+          goodsCount:1
+        }, {
+          goodsName: '爱心薯条',
+          goodsPrice: 8,
+          goodsCount:1
+        }, {
+          goodsName: '甜筒',
+          goodsPrice: 8,
+          goodsCount:1
+        }]
+      }
+    },
+    mounted: function () {
+     var orderHeight = document.body.clientHeight;
+     console.log(orderHeight);
+     document.getElementById('orderList').style.height = orderHeight + 'px';
+    }
+  }
+</script>
+
+<style scoped>
+  .pos-order {
+    background-color: #f9fafc;
+    border-right: 1px solid #c0ccda;
+  }
+  .btns {
+    margin-top: 15px;
+  }
+</style>
